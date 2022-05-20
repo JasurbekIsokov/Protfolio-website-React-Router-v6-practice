8448bf6d25ba82174a47369c1aad5414539791a2
Jasurbek <jasurisoqov685@gmail.com>
2022-05-20 06:41:22 +0500

    Contact RenderComponent

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SaidbarComponent from "./SaidbarComponent.js";
+import HeaderComponent from "./HeaderComponent.js";
+import ReturnComponent from "./homePage/ReturnComponent.js";
+import ReturnPortfolioComponent from "./portfolioPage/ReturnPortfolioComponent.js";
+import AboutRenderComponent from "./aboutPage/AboutRenderComponent.js";
+import ContactRenderComponent from "./contactPage/ContactRenderComponent.js";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="window">
+        <HeaderComponent />
+        <SaidbarComponent />
+        <div className="container">
+          <ReturnComponent />
+        </div>
+        <ReturnPortfolioComponent />
+        <AboutRenderComponent />
+        <ContactRenderComponent />
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App />, document.querySelector("#root"));
