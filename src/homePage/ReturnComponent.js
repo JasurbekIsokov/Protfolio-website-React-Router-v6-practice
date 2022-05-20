49bf3b26c9cc8fcf56cfbda39bbacb75ba1a8016
Jasurbek <jasurisoqov685@gmail.com>
2022-05-20 06:43:45 +0500

    Home Page Return Component

---
+++ src/homePage/ReturnComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import MainComponent from "./MainComponent.js";
+
+class ReturnComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <MainComponent />
+      </div>
+    );
+  }
+}
+
+export default ReturnComponent;
